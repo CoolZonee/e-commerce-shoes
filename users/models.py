53ea9e4749c412ea9daa152bca3f06ded31beefa
CoolZonee <ckeat0203@gmail.com>
2021-11-06 23:43:44 +0800

    Add username and password to User model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 
 class Position(models.Model):
@@ -10,11 +11,14 @@ class Position(models.Model):
 
 
 class User(models.Model):
+    usernmme = models.CharField(max_length=50, unique=True, primary_key=True)
+    password = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=1)
-    email = models.EmailField()
-    mobile_no = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    mobile_no = models.CharField(
+        max_length=20, unique=True, blank=False, null=False)
     dob = models.DateField(default=None)
     position = models.ForeignKey(Position, on_delete=models.PROTECT)
     joined_date = models.DateTimeField(auto_now_add=True)
